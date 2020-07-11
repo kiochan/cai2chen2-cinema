651e9dfeb8945d94b33191ee9c1e33e56a0b1403
kiochan <sunxingchen@live.com>
2020-07-11 15:25:28 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
-export default function IndexPage() {
-    return <div></div>
+import Container from '../components/Container'
+import Link from 'next/link'
+
+export default function IndexPage () {
+  return <Container>
+    <h1>Cinema Together</h1>
+    <Link href='/chen'>
+      <a>
+        <h3>ChenChen</h3>
+      </a>
+    </Link>
+    {' | '}
+    <Link href='/cai'>
+      <a>
+        <h3>CaiCai</h3>
+      </a>
+    </Link>
+  </Container>
 }
